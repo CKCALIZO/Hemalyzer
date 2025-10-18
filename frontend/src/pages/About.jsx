dc872bc03843aa1ad3b575ae69b7a594beb3c6d6
CKCALIZO <ckcalizo18@gmail.com>
2025-10-18 23:51:05 +0800

    added new elements to left column of homepage

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,8 +6,8 @@ export const About = () => {
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex grow flex-col items-start justify-start p-8">
-                <h1 className="text-5xl">About page</h1>
-                <p className="m2 p-2">Hemalyzer is a thesis project designed to assist in the classification of 
+                <h1 className="text-4xl">About page</h1>
+                <p className="my-4 p-3">Hemalyzer is a thesis project designed to assist in the classification of 
                     hematological diseases, particularly leukemia and its subtypes 
                     (AML, ALL, CML, CLL), using NAS-optimized YOLOv8 with attention-enhanced 
                     feature pyramids for ConvNeXt classification
